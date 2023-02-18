4d60d1b07ea324439f4efb9ef225fcbf9d285989
irhooshyar <123286611+irhooshyar@users.noreply.github.com>
2023-02-18 22:26:27 +0330

    Docs Subject Extractor New Version

--- es_scripts/IngestFullProfileAnalysisToElastic.py
+++ es_scripts/IngestFullProfileAnalysisToElastic.py
@@ -26,11 +26,11 @@ class FullProfileIndex(ES_Index):
             classification_subject = record['classification_subject']
             sentiment = record['sentiment']
 
-            persons_list = json.loads(record['persons'].replace(b"\'", b'\"'))
+            persons_list = json.loads(record['persons'].replace("'", '"'))
 
-            locations_list = json.loads(record['locations'].replace(b"\'", b'\"'))
+            locations_list = json.loads(record['locations'].replace("'", '"'))
 
-            organizations_list = json.loads(record['organizations'].replace(b"\'", b'\"'))
+            organizations_list = json.loads(record['organizations'].replace("'", '"'))
 
 
             persons = [item['word'] for item in persons_list] if len(persons_list) != 0 else ['بدون شخص حقیقی']
