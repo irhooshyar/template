54849864dba0f294c1d33f183a75813f20f291a8
irhooshyar <123286611+irhooshyar@users.noreply.github.com>
2023-02-25 15:29:33 +0330

    Docs Subject Extractor New Version

--- doc/views.py
+++ doc/views.py
@@ -105,10 +105,13 @@ def update_doc(request, id, language, ):
     file.save()
 
 
-    StratAutomating.apply.after_response(folder_name, file,
-                                             "IngestFullProfileAnalysisToElastic",
-                                             host_url)  # AdvanceARIMAExtractor_ ActorTimeSeriesPrediction _DocsSubjectExtractor_DocsLevelExtractor_DocsReferencesExtractor_DocsActorsTimeSeriesDataExtractor_DocsCreateDocumentsListCubeData_DocsCreateSubjectCubeData_DocsCreateVotesCubeData_DocsCreateSubjectStatisticsCubeData_DocsCreateTemplatePanelsCubeData_DocsAnalysisLeadershipSlogan_DocsCreatePrinciplesCubeData_DocCreateBusinessAdvisorCubeData_DocsCreateRegularityLifeCycleCubeData_DocsExecutiveParagraphsExtractor_DocsClauseExtractor_DocsGraphCubeData_DocsCreateMandatoryRegulationsCubeData_DocsExecutiveClausesExtractor_DocsCreateActorInformationStackChartCubeData
+    # StratAutomating.apply.after_response(folder_name, file,
+    #                                          "IngestFullProfileAnalysisToElastic",
+    #                                          host_url)  # AdvanceARIMAExtractor_ ActorTimeSeriesPrediction _DocsSubjectExtractor_DocsLevelExtractor_DocsReferencesExtractor_DocsActorsTimeSeriesDataExtractor_DocsCreateDocumentsListCubeData_DocsCreateSubjectCubeData_DocsCreateVotesCubeData_DocsCreateSubjectStatisticsCubeData_DocsCreateTemplatePanelsCubeData_DocsAnalysisLeadershipSlogan_DocsCreatePrinciplesCubeData_DocCreateBusinessAdvisorCubeData_DocsCreateRegularityLifeCycleCubeData_DocsExecutiveParagraphsExtractor_DocsClauseExtractor_DocsGraphCubeData_DocsCreateMandatoryRegulationsCubeData_DocsExecutiveClausesExtractor_DocsCreateActorInformationStackChartCubeData
+    #
 
+    from scripts.Persian import DocsParagraphVectorExtractor
+    DocsParagraphVectorExtractor.bulk_to_db(folder_name, file)
 
     # from scripts.Persian import DocsParagraphVectorExtractor
     # DocsParagraphVectorExtractor.apply(folder_name, file)
--- scripts/Persian/DocsParagraphVectorExtractor.py
+++ scripts/Persian/DocsParagraphVectorExtractor.py
@@ -18,90 +18,90 @@ import hazm
 import requests
 import time
 import json
+#
+# import torch
+# from sentence_transformers import models, SentenceTransformer, util
 
-import torch
-from sentence_transformers import models, SentenceTransformer, util
-
-
-    
-def load_st_model(model_name_or_path):
-    word_embedding_model = models.Transformer(model_name_or_path)
-    pooling_model = models.Pooling(
-        word_embedding_model.get_word_embedding_dimension(),
-        pooling_mode_mean_tokens=True,
-        pooling_mode_cls_token=False,
-        pooling_mode_max_tokens=False)
-    
-    model = SentenceTransformer(modules=[word_embedding_model, pooling_model])
-    return model
-
- # Load the Sentence-Transformer
-farstail_model = 'm3hrdadfi/bert-fa-base-uncased-farstail-mean-tokens'
-wikinli_model = 'm3hrdadfi/bert-fa-base-uncased-wikinli-mean-tokens'
-wikitriplet_model = 'm3hrdadfi/bert-fa-base-uncased-wikitriplet-mean-tokens'
-
-embedder = load_st_model(wikitriplet_model)
-
-def apply(folder_name, Country):
-    ParagraphVector.objects.filter(paragraph__document_id__country_id__id = Country.id).delete()
-    
-    batch_size = 1000
-
-    t = time.time()
-
-    # get country paragraphs
-    paragraph_list = DocumentParagraphs.objects.filter(document_id__country_id__id = Country.id)[:]
-    print(len(paragraph_list))
-
-    paragraph_dict = {}
-    i = 0
-    for para_obj in paragraph_list:
-        paragraph_dict[i] = para_obj
-        i += 1
-
-    # create corpus
-    corpus = []
-    for index,para_obj in paragraph_dict.items():
-        print(index)
-        corpus.append(para_obj.text)
-
-    t = time.time()
-    print('started...')
-
-    corpus_embeddings = embedder.encode(corpus,batch_size = 32, convert_to_tensor=False, show_progress_bar=True)
-    
-    print("time ", time.time() - t)
-
-    # save vectors to db
-    vector_type = None
-    try:
-        vector_type = ParagraphVectorType.objects.get(name = 'wikitriplet_vector')
-    except:
-        vector_type = ParagraphVectorType.objects.create(name = 'wikitriplet_vector')
-
-    create_list = []
-    for i in range(len(paragraph_list)):
-        para_vector = (list(corpus_embeddings[i]))
-        para_vector = [float(num) for num in para_vector]
-
-        row_obj = ParagraphVector(
-            paragraph = paragraph_dict[i],
-            vector_value = {"data":para_vector},
-            vector_type = vector_type
-        )
-        create_list.append(row_obj)
-
-
-        if create_list.__len__() > batch_size:
-            ParagraphVector.objects.bulk_create(create_list)
-            create_list = []
-
-
-    ParagraphVector.objects.bulk_create(create_list)
-
-    
-    IngestParagraphsVectorsToElastic.apply(folder_name,Country,0)
-
+#
+#
+# def load_st_model(model_name_or_path):
+#     word_embedding_model = models.Transformer(model_name_or_path)
+#     pooling_model = models.Pooling(
+#         word_embedding_model.get_word_embedding_dimension(),
+#         pooling_mode_mean_tokens=True,
+#         pooling_mode_cls_token=False,
+#         pooling_mode_max_tokens=False)
+#
+#     model = SentenceTransformer(modules=[word_embedding_model, pooling_model])
+#     return model
+#
+#  # Load the Sentence-Transformer
+# farstail_model = 'm3hrdadfi/bert-fa-base-uncased-farstail-mean-tokens'
+# wikinli_model = 'm3hrdadfi/bert-fa-base-uncased-wikinli-mean-tokens'
+# wikitriplet_model = 'm3hrdadfi/bert-fa-base-uncased-wikitriplet-mean-tokens'
+#
+# embedder = load_st_model(wikitriplet_model)
+#
+# def apply(folder_name, Country):
+#     ParagraphVector.objects.filter(paragraph__document_id__country_id__id = Country.id).delete()
+#
+#     batch_size = 1000
+#
+#     t = time.time()
+#
+#     # get country paragraphs
+#     paragraph_list = DocumentParagraphs.objects.filter(document_id__country_id__id = Country.id)[:]
+#     print(len(paragraph_list))
+#
+#     paragraph_dict = {}
+#     i = 0
+#     for para_obj in paragraph_list:
+#         paragraph_dict[i] = para_obj
+#         i += 1
+#
+#     # create corpus
+#     corpus = []
+#     for index,para_obj in paragraph_dict.items():
+#         print(index)
+#         corpus.append(para_obj.text)
+#
+#     t = time.time()
+#     print('started...')
+#
+#     corpus_embeddings = embedder.encode(corpus,batch_size = 32, convert_to_tensor=False, show_progress_bar=True)
+#
+#     print("time ", time.time() - t)
+#
+#     # save vectors to db
+#     vector_type = None
+#     try:
+#         vector_type = ParagraphVectorType.objects.get(name = 'wikitriplet_vector')
+#     except:
+#         vector_type = ParagraphVectorType.objects.create(name = 'wikitriplet_vector')
+#
+#     create_list = []
+#     for i in range(len(paragraph_list)):
+#         para_vector = (list(corpus_embeddings[i]))
+#         para_vector = [float(num) for num in para_vector]
+#
+#         row_obj = ParagraphVector(
+#             paragraph = paragraph_dict[i],
+#             vector_value = {"data":para_vector},
+#             vector_type = vector_type
+#         )
+#         create_list.append(row_obj)
+#
+#
+#         if create_list.__len__() > batch_size:
+#             ParagraphVector.objects.bulk_create(create_list)
+#             create_list = []
+#
+#
+#     ParagraphVector.objects.bulk_create(create_list)
+#
+#
+#     IngestParagraphsVectorsToElastic.apply(folder_name,Country,0)
+#
 
 def bulk_to_db(folder_name, Country):
     ParagraphVector.objects.filter(paragraph__document_id__country_id__id = Country.id).delete()
@@ -111,9 +111,9 @@ def bulk_to_db(folder_name, Country):
     t = time.time()
     print('started...')
 
-    #load json
-    with open('test_vector_result.json') as user_file:
-    parsed_json = json.load(user_file)
+    # load json
+    with open(r'G:\asre_iran\asre_iran_vector_result.json') as user_file:
+        parsed_json = json.load(user_file)
 
 
     # save vectors to db
@@ -123,12 +123,16 @@ def bulk_to_db(folder_name, Country):
     except:
         vector_type = ParagraphVectorType.objects.create(name = 'wikitriplet_vector')
 
+    create_list = []
+    cntr = 1
     for item in parsed_json:
+        print(cntr/parsed_json.__len__())
+        cntr +=1
         para_id = item
         para_vector = parsed_json[item]
 
         row_obj = ParagraphVector(
-            paragraph = paragraph_dict[i],
+            paragraph = para_id,
             vector_value = {"data":para_vector},
             vector_type = vector_type
         )
