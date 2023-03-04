7a15220ac43edbc81614f1f8be8d2ce5df366427
Alizadeh <33342806+Alizadeh275@users.noreply.github.com>
2023-03-04 08:32:00 +0330

    added sort

--- doc/views.py
+++ doc/views.py
@@ -3489,7 +3489,13 @@ def SearchDocument_ES(request, country_id, category_id, subject_id, from_year, t
                              query=res_query,
                              aggregations=res_agg,
                              from_=from_value,
-                             size=search_result_size
+                             size=search_result_size,
+                            sort=[
+                                 {"document_jalili_date.year": {"order": "desc"}},
+                                 {"document_jalili_date.month.number": {"order": "desc"}},
+                                 {"document_jalili_date.day.number": {"order": "desc"}},
+                                 {"document_jalili_date.hour": {"order": "desc"}},
+                             ]
 
                              )
 
