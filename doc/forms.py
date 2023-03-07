115500dfbe477c2708f41438ce1f9efbf41270e9
mhhasani <hasanimohammadhosein@gmail.com>
2023-03-07 16:09:59 +0330

    captcha added

--- doc/forms.py
+++ doc/forms.py
@@ -1,6 +1,6 @@
 from django import forms
-# from captcha.fields import ReCaptchaField
-# from captcha.widgets import ReCaptchaV2Checkbox
+from captcha.fields import ReCaptchaField
+from captcha.widgets import ReCaptchaV2Checkbox
 
 class ZipFileForm(forms.Form):
     docfile = forms.FileField(label='Select a file', help_text='max. 10 megabytes')
@@ -8,5 +8,4 @@ class ZipFileForm(forms.Form):
     language = forms.TextInput()
 
 class CaptchaTestForm(forms.Form):
-    # captcha = ReCaptchaField()
-    pass
+    captcha = ReCaptchaField()
