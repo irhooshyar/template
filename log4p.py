7e4a245ece9f73a5a3cd05db920f87fdaf3fafb5
irhooshyar <123286611+irhooshyar@users.noreply.github.com>
2023-03-08 19:29:47 +0330

    model named rename fixed

---
+++ log4p.py
@@ -0,0 +1,33 @@
+config = {
+    'monitorInterval': 10,                         # auto reload time interval [secs]
+    'loggers': {
+        'LogDemo': {
+            'level': "DEBUG",
+            'additivity': False,
+            'AppenderRef' : ['LogDemo']
+            },
+        'root': {
+            'level': "DEBUG",
+            'AppenderRef': ['output_root']
+        }
+    },
+    'appenders': {
+        'output_root': {
+            'type': "file",
+            'FileName': "error.log",            # log file name
+            'backup_count': 5,                       # files count use backup log
+            'file_size_limit': 1024 * 1024 * 20,     # single log file size, default :20MB
+            'PatternLayout': "[level:%(levelname)\ts-file:%(filename)\ts-lineno:%(lineno)d]\t%(asctime)s\t%(message)s"
+        },
+        'LogDemo': {
+            'type': "file",
+            'FileName': "LogDemo.log",
+            'PatternLayout': "[level:%(levelname)\ts-file:%(filename)\ts-lineno:%(lineno)d]\t%(asctime)s\t%(message)s"
+        },
+        'console': {
+            'type': "console",
+            'target': "console",
+            'PatternLayout': "[%(levelname)s]\t%(asctime)s\t%(message)s"
+        }
+    }
+}
