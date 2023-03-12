5d3c9a9e1d654da7eb3fc7756a81d8f7b156453d
irhooshyar <123286611+irhooshyar@users.noreply.github.com>
2023-03-12 16:08:58 +0330

    logging add

---
+++ abdal/log_config.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+import os
+import logging
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+LOGGING_PATH = os.path.join(BASE_DIR, 'log', "template.log")
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# create a file handler and set its level to DEBUG
+file_handler = logging.FileHandler(LOGGING_PATH)
+file_handler.setLevel(logging.DEBUG)
+
+# create a console handler and set its level to ERROR
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.ERROR)
+
+# create a formatter and set it to the handlers
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+console_handler.setFormatter(formatter)
+
+# add the handlers to the logger
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
+
+
+def insert_log(message, file_name, line, level):
+    if level in ("debug", "Debug", "DEBUG"):
+        logger.debug(f'{message} (File: {file_name}, Line:{line})')
+    if level in ("info", "Info", "INFO"):
+        logger.info(f'{message} (File: {file_name}, Line:{line})')
+    if level in ("warning", "Warning", "WARNING"):
+        logger.warning(f'{message} (File: {file_name}, Line:{line})')
+    if level in ("error", "Error", "ERROR"):
+        logger.error(f'{message} (File: {file_name}, Line:{line})')
