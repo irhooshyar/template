75a1d7b9db0443952c0d61dc4ea489baab152d63
Parinaz <parisoltan77@gmail.com>
2023-03-13 23:21:42 +0330

    add new edits

--- doc/static/js/CustomAlert.js
+++ doc/static/js/CustomAlert.js
@@ -1,63 +1,59 @@
 
-const html_value = `<button type="button" id="HooshyarModalBtn" class="btn d-none modal_btn" data-bs-toggle="modal"
-                    data-bs-target="#HooshyarModal"></button>
-                    <div class="modal fade" id="HooshyarModal" tabindex="-1" role="dialog" aria-labelledby="HooshyarModalHeader" aria-hidden="true">
-                        <div class="modal-dialog" role="document">
-                          <div class="modal-content">
-                            <div class="modal-header">
-                                <div class="d-flex" style="flex-direction: row-reverse;">
-                                    <div class="icon-parent" id="HooshyarModalIconParent">
-                                        <i class="icon-child fa fa-exclamation" id="HooshyarModalIcon"></i>
-                                    </div>
-                                    <h5 class="modal-title" id="HooshyarModalHeader">
-                                    </h5>
-                                </div>
-                              <button type="button" class="closeset closeModal" data-dismiss="modal" aria-label="Close" style="color: #4F4F4F;">
-                              &times;
-                              </button>
-                            </div>
-                            <div class="modal-body" id="HooshyarModalBody">
-                            </div>
-                            <div class="modal-footer" style="text-align: center; justify-content: center">
-                              <button type="button" class="btn btn-secondary btn-understand closeModal" data-dismiss="modal">متوجه شدم</button>
-                            </div>
-                          </div>
-                        </div>
-                    </div>`
 
-setTimeout(() => {
+function HooshyarAlertShow(alert_header_text, alert_body_text, icon_type) {
+  let icon_parent_color = "#FDCECE"
+  let icon_color = "#F30C0C"
+  if (icon_type === 2)
+  {
+      icon_parent_color = "#FEEED7"
+      icon_color = "#C27605"
+  }
+
+  alert_body_text = alert_body_text || '';
+  const html_value = `<div class="alert" tabindex="-1" role="dialog" aria-labelledby="HooshyaralertHeader" aria-hidden="true">
+    <div class="alert-header"  >
+      <div class="d-flex" style="flex-direction: row-reverse;">
+          <div class="icon-parent" style="background: ${icon_parent_color}">
+              <i class="icon-child fa fa-exclamation" style="background: ${icon_color}"></i>
+          </div>
+          <h5 class="alert-title">
+            ${alert_header_text}
+          </h5>
+      </div>
+      <button type="button" class="closeset" aria-label="Close" style="color: #4F4F4F;" onClick=HooshyarAlertClose()>
+        <i class="fa fa-remove" ></i>
+      </button>
+    </div>
+    <div class="alert-body" id="alertBody" style="text-align: center; justify-content: center">
+      ${alert_body_text}
+    </div>
+    <div class="alert-footer" id="alertFooter" style="text-align: center; justify-content: center">
+      <button type="button" id="alertBtn" class="btn btn-secondary btn-understand" onClick=HooshyarAlertClose()>متوجه شدم</button>
+    </div>
+  </div>`
+
+  
+
   document.getElementById("HooshyarAlert").innerHTML = html_value
-}, "1000")
+  if (alert_body_text == "ثبت‌نام شما توسط ادمین تایید شده است. از طریق دکمه‌ی زیر، وارد سامانه شوید."){
+    let tag = '<a href="http://virtualjuristic.datakaveh.com:7090/login/"><button type="button" id="login-btn" class="btn btn-secondary btn-understand closeModal" data-dismiss="modal">ورود</button></a>';
+    document.getElementById("alertFooter").innerHTML += tag;
+    document.getElementById("alertBtn").style.display = "none";
+  }
+  if (alert_body_text == "متاسفانه تایید شما توسط ادمین رد شده است. با پشتیبانی از طریق ایمیل زیر، در تماس باشید."){
+    let tag = '<div class="alert-body">support@irhooshyar.com</div>';
+    document.getElementById("alertBody").innerHTML += tag;
+  }
+  if (alert_body_text == "اگر ثبت‌نام را انجام نداده‌اید، بر روی دکمه‌ی زیر کلیک فرمایید."){
+    let tag = '<a href="http://virtualjuristic.datakaveh.com:7090/signup/"><button type="button" id="login-btn" class="btn btn-secondary btn-understand closeModal" data-dismiss="modal">ثبت‌نام</button></a>';
+    document.getElementById("alertFooter").innerHTML += tag;
+    document.getElementById("alertBtn").style.display = "none";
+  }
+  document.getElementById("HooshyarAlert").style.display = 'flex';
 
+}
 
-function HooshyarAlertShow(alert_header_text, alert_body_text, icon_type)
-{
-
-    let icon_parent_color = NaN
-    let icon_color = NaN
-    if (icon_type === 1)
-    {
-        icon_parent_color = "#FDCECE"
-        icon_color = "#F30C0C"
-    }
-    else if (icon_type === 2)
-    {
-        icon_parent_color = "#FEEED7"
-        icon_color = "#C27605"
-    }
-
-    document.getElementById("HooshyarModalIconParent").style.backgroundColor = icon_parent_color
-    document.getElementById("HooshyarModalIcon").style.backgroundColor = icon_color
-    document.getElementById("HooshyarModalHeader").innerText = alert_header_text
-    document.getElementById("HooshyarModalBody").innerText = alert_body_text
-
-    const closeModalBtns = document.getElementsByClassName("closeModal")
-    for (row of closeModalBtns) {
-        row.onclick = function () {
-            $("#HooshyarModal").modal("hide")
-        }
-    }
-
-    $("#HooshyarModalBtn").click()
-
+function HooshyarAlertClose() {
+document.getElementById("HooshyarAlert").innerHTML = ""
+document.getElementById("HooshyarAlert").style.display = 'none';
 }
